ebf81c5490bc48c92a9ad61c11b8d2d4a8d7f9c5
WMorris-WebDev20 <william.morris@bryanuniversity.edu>
2021-04-28 14:09:36 -0500

    added capstone

---
+++ week7/week7capstone/test/walmartTest.js
@@ -0,0 +1,87 @@
+const assert = require('chai').assert
+const expect = require('chai').expect
+const Walmart = require('../walmart')
+
+describe("Testing Instance Of The Cart", () => {
+    const checkout = new Walmart()
+
+    it('Should be an instance of Walmart', () =>{
+        expect(checkout).to.be.instanceOf(Walmart)
+    } )
+
+})
+
+describe('Testing Add Item', () => {
+    const checkout = new Walmart()
+    it('Should add item', ()=>{
+        checkout.addItem("Cheese", 3.99)
+        assert.equal(checkout.item.filter(item => item.name === "Cheese").map(item => item.name), "Cheese")
+    })
+    
+    describe('Testing Add Price to Item', ()=> {
+
+        it('Should add or change price to item', () => {
+            assert.equal(checkout.item.filter(item => item.name === 'Cheese').map(item => item.price), 3.99)
+            checkout.addPrice('Cheese',2.15)
+            assert.equal(checkout.item.filter(item => item.name === 'Cheese').map(item => item.price), 2.15)
+        })
+
+    })
+})
+
+
+describe('Testing Adding To The Cart', () => {
+    const checkout = new Walmart()
+    it('Should add items to the cart', () => {
+        
+        checkout.addItem('Milk', 2.99)
+        checkout.addItem("Chicken", 2.99)
+        checkout.addItem('Ground Beef', 5.99)
+        checkout.addItem('Buns', 1.99)      
+        checkout.addToCart("Chicken",10)
+        checkout.addToCart("Milk", 2)
+        checkout.addToCart("Ground Beef", 2)
+        checkout.addToCart("Buns", 3)
+        
+    })
+    describe('Should Get Cart', () => {
+        it('Gets Items In Cart', () => {            
+            checkout.getCart()         
+        })    
+    })
+
+    it("Should get total in cart", ()=>{
+        checkout.getTotal()
+        assert.equal(checkout.getTotal(),53.83)        
+    })
+    
+    describe('Testing Error Thrown', () => {
+        it('Should Throw Not An Item Error', () => {            
+            expect(() => checkout.addToCart('Not Chicken', 1)).to.throw();            
+        }) 
+        it('Should Throw No Price Error', () => {            
+            expect(() => checkout.addItem('Not Chicken')).to.throw();            
+        })    
+    })
+
+})
+
+describe('Testing Adding Discounts', () => {
+    const checkout = new Walmart()
+    describe('Testing Should Add Discount', () => {
+        it('Should add discounts', () => {
+            checkout.addDiscount("Chicken", 5 , .5)
+            checkout.addDiscount("Ground Beef",1 , .75) 
+            checkout.addItem("Chicken", 2 )
+            checkout.addItem('Ground Beef', 5.99)     
+            checkout.addToCart("Chicken",10)
+            checkout.addToCart("Ground Beef", 2)
+            assert.equal(checkout.cart.filter(item => item.name === 'Chicken').map(item => item.price), 10)
+            assert.equal(checkout.cart.filter(item => item.name === 'Ground Beef').map(item => item.price), 8.99)
+            
+        })
+    
+    })
+
+})
+
---
+++ week7/week7capstone/walmart.js
@@ -0,0 +1,95 @@
+class Walmart{
+
+    constructor(){
+        this.cart = []   
+        this.discount = []
+        this.item = [] 
+    }
+
+    // Adds items to the cart
+    addToCart = (product, count) => {
+        let check = this.cart.map(item => item.name).indexOf(product)
+
+        let qty = this.cart.filter(item => item.name === product? item.count: 0 )
+        let newQty = qty.map(item => item.count)
+
+        let productCost = this.item.find(val => val.name === product)
+        if(check != -1 && productCost != undefined){
+
+            let newDiscount = this.calculateDiscount(product, count)            
+            let cost = this.item.map(item => item.name === product ? item.price : null )
+            let itemPrice = Math.round((productCost.price * newDiscount * (count + newQty[0]) ) * 100 )/ 100
+            this.cart[check] = {name: product, price: itemPrice, count: newQty[0] + count}
+
+        }else if(check === -1 && productCost != undefined){
+
+            let newDiscount = this.calculateDiscount(product, count)
+            let itemPrice = Math.round((productCost.price * newDiscount * count) * 100 )/ 100
+            this.cart = [ ...this.cart , {name: product, price: itemPrice, count: count }]           
+
+        }
+        else{
+            throw new Error('Not an item')
+        }
+
+        return this.cart
+
+
+    }
+    //Returns the total of the cart
+    getTotal = () => {
+        let total = 0
+        this.cart.map(item => total += item.price)
+
+        return total
+    }
+
+    //Adds an item to the item array
+    addItem = (product, price) => {
+        if (price != undefined){
+        this.item = [...this.item, {name: product, price: price}]
+        }else{
+            throw new Error("No price")
+        }
+
+    }
+
+    // Returns the item array
+    getItems = () => {
+        return this.item
+    }
+
+    //Returns the contents of the cart
+    getCart = () => {
+        return this.cart
+    }
+
+    //Adds or changes a price of an item
+    addPrice = (product, price) => {
+
+        let check = this.item.map(item => item.name).indexOf(product)
+
+        if (check != 1){
+            this.item[check].price = price
+        }
+
+    }
+
+    //Adds Discount rules
+    addDiscount = (item, count, disc) => {
+        this.discount = [...this.discount, {name: item , count: count, discount: disc}]
+        
+    }
+
+    //Calculates Discount
+    calculateDiscount = (item, count) => {
+        
+        let dis = this.discount.filter(disc => disc.name === item && disc.count <= count
+        )
+          return dis!= 0 ? dis[0].discount : 1
+      }
+
+
+}
+
+module.exports = Walmart
